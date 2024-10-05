6b7e614e3487334332df271b72dea95ae4dc3c55
KarolisVaig <karolis.vaiginis@knf.stud.vu.lt>
2024-10-05 12:05:44 +0100

    pirmas mygtukas ir tekstas

--- app/src/main/java/com/example/projektas3/MainActivity.kt
+++ app/src/main/java/com/example/projektas3/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.projektas3
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button3)
+        val textView: TextView = findViewById(R.id.textView)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        button.setOnClickListener {
+            textView.text = "Naujas tekstas!"
         }
     }
 }
