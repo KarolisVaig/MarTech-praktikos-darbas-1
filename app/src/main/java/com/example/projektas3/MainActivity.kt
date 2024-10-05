a0adb0c59b82fe0c42a17de68eb4e5f5ef02320c
KarolisVaig <karolis.vaiginis@knf.stud.vu.lt>
2024-10-05 12:16:56 +0100

    Mygtukas pakeisti spalva

--- app/src/main/java/com/example/projektas3/MainActivity.kt
+++ app/src/main/java/com/example/projektas3/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.projektas3
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -9,12 +10,16 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val colors = listOf(Color.RED, Color.BLUE, Color.GREEN, Color.MAGENTA, Color.YELLOW)
+    private var currentColorIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.button3)
+        val button3: Button = findViewById(R.id.button3)
+        val button4: Button = findViewById(R.id.button4)
         val textView: TextView = findViewById(R.id.textView)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -23,8 +28,16 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        button.setOnClickListener {
+        button3.setOnClickListener {
             textView.text = "Naujas tekstas!"
+        }
+
+        button4.setOnClickListener {
+            // Change the text color to the next color in the list
+            textView.setTextColor(colors[currentColorIndex])
+
+            // Update the index for the next color
+            currentColorIndex = (currentColorIndex + 1) % colors.size
         }
     }
 }
